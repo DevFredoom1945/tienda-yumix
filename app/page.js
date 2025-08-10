2d121e78809338c0ee71856bfbcf61d00f978ee3
DevFredoom1945 <moshecacua@gmail.com>
2025-08-10 17:02:00 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import HeroPromo from '../components/HeroPromo';
 import Benefits from '../components/Benefits';
 import Categories from '../components/Categories';
@@ -14,13 +13,6 @@ export default function Home() {
 
       {/* Categorías del mes */}
       <Categories />
-
-      {/* Contenido original */}
-      <h1 style={{ fontSize: 28, marginBottom: 8 }}>¡Bienvenido!</h1>
-      <p style={{ color: '#444' }}>Starter limpio listo para publicar en Vercel.</p>
-      <p>
-        <Link href="/catalogo">Ir al catálogo →</Link>
-      </p>
     </section>
   );
 }
