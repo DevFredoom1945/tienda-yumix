0c2a33ba024131d8327eea4f3c34263fa71bef71
DevFredoom1945 <moshecacua@gmail.com>
2025-08-11 14:26:25 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default async function RootLayout({ children }) {
           <div className="container topbar-inner">
             <div className="brand">
               {/* Renombra el archivo a /public/logo-2x.png */}
-              <img src="/logo-2x.png" alt="Yumix" style={{ height: 64, objectFit: 'contain' }} />
+              <img src="/logo 2x.png" alt="Yumix" style={{ height: 64, objectFit: 'contain' }} />
             </div>
 
             <form className="search" action="/buscar" method="GET">
